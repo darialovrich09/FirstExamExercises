dc6254e6324081126bdcd33d4cd446cba3817cd0
darialovrich09 <darialovrich09@gmail.com>
2026-02-08 10:00:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.Festival
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rent = int.Parse(Console.ReadLine());
+
+            double decoration = rent * 0.25;
+            double sound = decoration + 50;
+            double security = 0.15 * (rent + sound);
+            double cleanup = 0.1 * rent;
+            double totalPrice = rent + decoration + sound + security + cleanup;
+
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
